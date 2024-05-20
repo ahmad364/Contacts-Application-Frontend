fcd1fad62c34e96ab114084f40b6c327ade960ae
ahmad364 <ahmadfiverr03034@gmail.com>
2024-05-20 20:15:34 +0500

    Code refactored

--- src/Components/PageContainer/PageContainer.jsx
+++ src/Components/PageContainer/PageContainer.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import './ContainerStyles.css'
+import './Styles.css'
+
 function PageContainer(props) {
+
+  // Define the class name conditionally
+  const className = props.containerFor === 'table' ? 'table-container' : 'page-container';
+
   return (
-    <div className='page-container'>
-        {props.children}
+    <div className={className}>
+      {props.children}
     </div>
   );
 }
--- src/Pages/ContactList.jsx
+++ src/Pages/ContactList.jsx
@@ -1,91 +1,109 @@
 import React, { useState } from 'react';
-import { IconButton } from '@material-ui/core';
-import DeleteIcon from '@material-ui/icons/Delete';
+import {
+  Delete as DeleteIcon,
+  Edit as EditIcon,
+  FilterList as FilterListIcon,
+  ArrowUpward as ArrowUpwardIcon,
+  Clear as ClearIcon,
+  Search as SearchIcon,
+  ArrowBack as ArrowBackIcon,
+  ArrowForward as ArrowForwardIcon,
+  ChevronRight as ChevronRightIcon,
+  ChevronLeft as ChevronLeftIcon
+} from '@material-ui/icons';
 import MaterialTable from 'material-table';
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
-import ClearIcon from '@material-ui/icons/Clear';
-import SearchIcon from '@material-ui/icons/Search';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import TableContainer from '../Components/PageContainer/TableContainer';
-import { Edit, FilterList } from '@material-ui/icons';
+import PageContainer from '../Components/PageContainer/PageContainer';
 import { useNavigate } from 'react-router-dom';
 
-function ContactList() {
-    
-    const navigate = useNavigate();
+const ContactList = () => {
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([
+    {
+      id: 1,
+      name: 'John',
+      lastName: 'Doe',
+      email: 'john@example.com',
+      phoneNumbers: ['1234567890', '9876543210']
+    },
+    {
+      id: 2,
+      name: 'Jane',
+      lastName: 'Doe',
+      email: 'jane@example.com',
+      phoneNumbers: ['1112223333', '4445556666']
+    }
+  ]);
 
-    const [contacts, setContacts] = useState([
-        {
-            id: 1,
-            name: 'John',
-            lastName: 'Doe',
-            email: 'john@example.com',
-            phoneNumbers: ['1234567890', '9876543210']
-        },
-        {
-            id: 2,
-            name: 'Jane',
-            lastName: 'Doe',
-            email: 'jane@example.com',
-            phoneNumbers: ['1112223333', '4445556666']
-        }
-    ]);
+  const tableIcons = {
+    Search: SearchIcon,
+    ResetSearch: ClearIcon,
+    SortArrow: ArrowUpwardIcon,
+    FirstPage: ArrowBackIcon,
+    LastPage: ArrowForwardIcon,
+    NextPage: ChevronRightIcon,
+    PreviousPage: ChevronLeftIcon,
+    Filter: FilterListIcon
+  };
 
-    return (
-        <TableContainer>
-            <MaterialTable
-                title="Contacts List"
-                icons={{
-                    Search: SearchIcon,
-                    ResetSearch: ClearIcon,
-                    SortArrow: ArrowUpwardIcon,
-                    ResetSearch: ClearIcon,
-                    FirstPage: ArrowBackIcon,
-                    LastPage: ArrowForwardIcon,
-                    NextPage: ChevronRightIcon,
-                    PreviousPage: ChevronLeftIcon,
-                    Filter: FilterList // Define filter icon
-                }}
-                columns={[
-                    { title: 'Name', field: 'name' },
-                    { title: 'Last Name', field: 'lastName' },
-                    { title: 'Email', field: 'email', filtering: true},
-                    {
-                        title: 'Phone Numbers',
-                        field: 'phoneNumbers',
-                        render: rowData => (
-                            <div>
-                                {rowData.phoneNumbers.map((phone, idx) => (
-                                    <div key={idx}>{phone}</div>
-                                ))}
-                            </div>
-                        )
-                    }
-                ]}
-                data={contacts}
-                actions={[
-                    {
-                        icon: () => <Edit color='primary' />,
-                        tooltip: 'Edit Contact',
-                        onClick: (event, rowData) => navigate(`/update/${rowData.id}`)
-                    },
-                    {
-                        icon: () => <DeleteIcon color='secondary' />,
-                        tooltip: 'Delete Contact',
-                        onClick: (event, rowData) => navigate(`/delete/${rowData.id}`)
-                    }
-                ]}
-                options={{
-                    actionsColumnIndex: -1,
-                    filtering: true
-                }}
-            />
-        </TableContainer>
-    );
-}
+  const columns = [
+    { title: 'Name', field: 'name' },
+    { title: 'Last Name', field: 'lastName' },
+    { title: 'Email', field: 'email', filtering: true },
+    {
+      title: 'Phone Numbers',
+      field: 'phoneNumbers',
+      render: rowData => (
+        <div>
+          {rowData.phoneNumbers.map((phone, idx) => (
+            <div key={idx}>{phone}</div>
+          ))}
+        </div>
+      )
+    }
+  ];
+
+  const actions = [
+    {
+      icon: () => <EditIcon color='primary' />,
+      tooltip: 'Edit Contact',
+      onClick: (event, rowData) => navigate(`/update/${rowData.id}`)
+    },
+    {
+      icon: () => <DeleteIcon color='secondary' />,
+      tooltip: 'Delete Contact',
+      onClick: (event, rowData) => navigate(`/delete/${rowData.id}`)
+    }
+  ];
+
+  const options = {
+    actionsColumnIndex: -1,
+    filtering: true,
+    headerStyle: {
+      paddingTop: 0,
+      paddingBottom: 0,
+      whiteSpace: 'nowrap',
+      backgroundColor: '#3f51b5',
+      color: '#fff',
+      height: '50px',
+      position: 'sticky',
+      top: 0,
+      overflow: 'hidden',
+      zIndex: 2
+    }
+  };
+
+  return (
+    <PageContainer containerFor="table">
+      <MaterialTable
+        title="Contacts List"
+        icons={tableIcons}
+        columns={columns}
+        data={contacts}
+        actions={actions}
+        options={options}
+      />
+    </PageContainer>
+  );
+};
 
 export default ContactList;
